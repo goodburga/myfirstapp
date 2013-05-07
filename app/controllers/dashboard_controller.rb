283f21c6d8a1364765fc50c9880fc2b2e0a4681b
Paul Burger <paul@skyscraper.io>
2013-05-07 07:59:37 -0700

    Initial commit

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,4 @@
+class DashboardController < ApplicationController
+  def hello_world
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyFirstApp::Application.config.secret_token = '5ad0b7aedd33d4c96f89701047f408eb953176b42cabaf185faff0b1f5cf61471eba879ab949e98e4b94aa3fac4bfda5396654f92d12d32586437c0e42dce3e1'
